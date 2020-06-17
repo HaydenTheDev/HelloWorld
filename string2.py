4eeaac8b3440da894ef7eb54dabb16be81dcb0b3
Hayden Bazor <hbazor@murraystate.edu>
2020-06-17 14:29:55 -0500

    more index and slicing and using a step in a slice.

--- string2.py
+++ string2.py
@@ -2,29 +2,13 @@
 #         01234567890123
 parrot = "Norwegian Blue"
 
-print(parrot)
+#  basically its 6 / 2 or 6 / 3
+print(parrot[0:6:2])  #  Nre 0 2 4
+print(parrot[0:6:3])  #  Nw 0 3
 
-print(parrot[3])
-print(parrot[4])
-print(parrot[9])
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
+number = "9,223;372:036 854,775;807"
+seperator = number[1::4]
+print(seperator)
 
-print()
-
-print(parrot[-11])
-print(parrot[-10])
-print(parrot[-5])
-print(parrot[-11])
-print(parrot[-8])
-print(parrot[-6])
-
-print()
-
-print(parrot[3 - 14])
-print(parrot[4 - 14])
-print(parrot[9 - 14])
-print(parrot[3 - 14])
-print(parrot[6 - 14])
-print(parrot[8 - 14])
+values = "".join(char if char not in seperator else " " for char in number).split()
+print([int(val) for val in values])
