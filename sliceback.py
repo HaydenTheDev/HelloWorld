346a69dae1e8cd34cab778d4b2b46f86ae65243a
Hayden Bazor <hbazor@murraystate.edu>
2020-06-17 15:10:11 -0500

    more slices

---
+++ sliceback.py
@@ -0,0 +1,14 @@
+#          012345678901234567890123456
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+chars = letters[16:13:-1]
+print(chars)
+intro = letters[4::-1]
+print(intro)
+last = letters[26:17:-1]
+print(last)
+
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])
