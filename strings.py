9a32176aaa6e316593567e80a2e9a9a1eb006f4b
Hayden Bazor <hbazor@murraystate.edu>
2020-06-16 21:55:07 -0500

    Learning more strings in Python

--- strings.py
+++ strings.py
@@ -3,3 +3,7 @@ print("Python is fun")
 print("Python strings are easy to learn")
 print('We can actually put "strings" inside of strings')
 print("hello" + "world")
+greeting = "Hello"
+name = input("Please enter your name: ")
+#This is a comment
+print(greeting + ' ' + name) 
