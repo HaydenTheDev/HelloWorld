4e2f463500387b2f0fe726a95f24392cd48fb6c4
Hayden Bazor <hbazor@murraystate.edu>
2020-06-18 10:37:47 -0500

    if Statements

--- blocks.py
+++ blocks.py
@@ -1,6 +1,15 @@
 
 #  if Statements
 name = input("Please enter your name: ")
-age = int(input("How old are you, {0}? ".format(name)))
+age = int(input("How old are you, {0}?".format(name)))
 if age >= 18:
     print("{0} old is enough to vote!".format(name))
+else:
+    print("Sorry! {0}you are not of age to vote.".format(name))
+if age < 18:
+    print("Please come back in {0} years.".format(18 - age))
+elif age == 900:
+    print("Sorry, Yoda, you die in return of the jedi.")
+else:
+    print("you are old enough to vote")
+    print("please out an X in the box")
